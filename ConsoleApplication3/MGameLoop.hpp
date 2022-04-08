be1e5ab11bdbc93de15fa109a952fcaa439895e7
재현 한 <0501wogus@naver.com>
2022-04-09 01:42:44 +0900

    [ES] FPS 출력

--- ConsoleApplication3/MGameLoop.hpp
+++ ConsoleApplication3/MGameLoop.hpp
@@ -12,10 +12,15 @@ namespace MuSeoun_Engine
 	private:
 		bool _isGameRunning;
 		MConsoleRenderer cRenderer;
+		int i = 0;
+		double time = 0;
 
 	public:
-		MGameLoop() { _isGameRunning = false; }
-		~MGameLoop() {}
+		MGameLoop() 
+		{ _isGameRunning = false; }
+		~MGameLoop() 
+		{
+		}
 
 		void Run()
 		{
@@ -60,19 +65,43 @@ namespace MuSeoun_Engine
 		{
 
 		}
+		 
 		void Render()
 		{
 			chrono::system_clock::time_point startRenderTimePoint = chrono::system_clock::now();
 
-			cRenderer.Clear();
+			
 			cRenderer.MoveCursor(10, 20);
-
+			cRenderer.Clear();
 
 			chrono::duration<double> renderDuration = chrono::system_clock::now() - startRenderTimePoint;
+			
+			
+			
+		    i++;
+			
+			time = time + renderDuration.count();
 
-			string fps = "FPS(milliseconds) : " + to_string(renderDuration.count());
-			cRenderer.DrawString(fps);
+			if (time >= 1.0f)  
+			{
+				string fps = "FPS(seconds) : " + to_string(i);
+				cRenderer.DrawString(fps);
+				time = 0;
+				i = 0;
+			}
+			
+			
+
+			//5string fps = "FPS(milliseconds) : " + to_string(renderDuration.count());
+			
+			
+
+			
+			//cRenderer.DrawString(fps);
 		}
+		
+		
+	
 
 
 		////cout << "Rendering speed : " << renderDuration.count() << "sec" << endl;
